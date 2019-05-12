eda8d28531dcae1f9c46d07e59fce0f0539a2eac
Yushi Yang <yangyushi1992@icloud.com>
2019-05-12 09:15:42 +0100

    new epipolar checker gui design

--- gui/epipolar.py
+++ gui/epipolar.py
@@ -1,3 +1,4 @@
+import re
 import fish_3d as f3
 import sys
 from PyQt5.QtWidgets import QWidget, QMainWindow, QPushButton, QLabel, QGridLayout, QApplication, QHBoxLayout, QLineEdit
@@ -8,109 +9,106 @@ from pyqtgraph.Qt import QtCore, QtGui
 pg.setConfigOptions(imageAxisOrder='row-major')
 
 class Model():
-    def __init__(self):
-        pass
+    def __init__(self, environment):
+        self.image_1 = None 
+        self.image_2 = None 
+        self.camera_1 = None 
+        self.camera_2 = None 
+        self.env = environment
+
+    @property
+    def is_valid(self):
+        return None not in (self.image_1, self.image_2, self.camera_1, self.camera_2)
+
+    @property
+    def water_level(self):
+        return float(self.env['z'].text())
+
+    @property
+    def normal(self):
+        normal_str =  self.env['n'].text()
+        normal = re.split(r'[\s,]+', n_str)
+        normal = [int(n) for n in normal]
+        return normal
+
+    @property
+    def depth(self):
+        return float(self.env['depth'].text())
+
+    def get_ep12(self, uv):
+        if self.is_valid:
+            return f3.ray_trace.epipolar_draw(
+                    uv, self.camera_1, self.camera_2, self.image_2, self.water_level, self.depth, self.normal
+                    )
 
 class Viewer(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.model = Model()
-
         self.layout = QGridLayout()
         self.window = QWidget()
         self.__setup()
+        env = {'z': self.edit_interface, 'n': self.edit_normal, 'depth': self.edit_depth}
+        self.model = Model(env)
 
     def __setup(self):
         self.window.setLayout(self.layout)
         self.setCentralWidget(self.window)
         self.__setup_left()
         self.__setup_right()
+        self.__setup_pannel()
         self.show()
 
     def __setup_left(self):
-        window = pg.GraphicsLayoutWidget(show=True, border=True)
-        window.setWindowTitle('Measuring ROI')
-
-        view = window.addViewBox(row=0, col=0, lockAspect=True)
-
-        #view.setRange(QtCore.QRectF(0, 0, image.shape[1], image.shape[0]))
-        #view.setLimits(xMin=0, yMin=0, xMax=image.shape[1], yMax=image.shape[0])
-        canvas = pg.ImageItem()
-        view.addItem(canvas)
-
-        self.left_canvas = canvas
-
-        self.layout.addWidget(window, 0, 0)
-
         pannel = QWidget()
         layout = QGridLayout()
         pannel.setLayout(layout)
+        window = pg.GraphicsLayoutWidget(show=True, border=True)
+        window.setWindowTitle('Measuring ROI')
+        view = window.addViewBox(row=0, col=0, lockAspect=True)
+        canvas = pg.ImageItem()
+        view.addItem(canvas)
+        self.left_canvas = canvas
         self.btn_load_image = QPushButton('Load Image')
         self.btn_load_camera = QPushButton('Load Camera')
-
-        label_interface  = QLabel('Water Level')
-        label_normal  = QLabel('Normal Direction')
-        label_step  = QLabel('Step')
-
-        self.left_edit_interface  = QLineEdit('0')
-        self.left_edit_normal  = QLineEdit('0, 0, 1')
-        self.left_edit_step  = QLineEdit('1')
-
-        layout.addWidget(self.btn_load_image, 0, 0)
-        layout.addWidget(self.btn_load_camera, 0, 1)
-
-        layout.addWidget(label_interface, 1, 0)
-        layout.addWidget(self.left_edit_interface, 1, 1)
-
-        layout.addWidget(label_normal, 2, 0)
-        layout.addWidget(self.left_edit_normal, 2, 1)
-
-        layout.addWidget(label_step, 3, 0)
-        layout.addWidget(self.left_edit_step, 3, 1)
-
-        self.layout.addWidget(pannel, 1, 0)
+        layout.addWidget(window, 0, 0, 1, 2)
+        layout.addWidget(self.btn_load_image, 1, 0)
+        layout.addWidget(self.btn_load_camera, 1, 1)
+        self.layout.addWidget(pannel, 0, 0)
 
     def __setup_right(self):
-        window = pg.GraphicsLayoutWidget(show=True, border=True)
-        window.setWindowTitle('Measuring ROI')
-
-        view = window.addViewBox(row=0, col=0, lockAspect=True)
-
-        #view.setRange(QtCore.QRectF(0, 0, image.shape[1], image.shape[0]))
-        #view.setLimits(xMin=0, yMin=0, xMax=image.shape[1], yMax=image.shape[0])
-        canvas = pg.ImageItem()
-        view.addItem(canvas)
-
-        self.right_canvas = canvas
-
         pannel = QWidget()
         layout = QGridLayout()
         pannel.setLayout(layout)
+        window = pg.GraphicsLayoutWidget(show=True, border=True)
+        window.setWindowTitle('Measuring ROI')
+        view = window.addViewBox(row=0, col=0, lockAspect=True)
+        canvas = pg.ImageItem()
+        view.addItem(canvas)
+        self.right_canvas = canvas
         self.btn_load_image = QPushButton('Load Image')
         self.btn_load_camera = QPushButton('Load Camera')
+        layout.addWidget(window, 0, 0, 1, 2)
+        layout.addWidget(self.btn_load_image, 1, 0)
+        layout.addWidget(self.btn_load_camera, 1, 1)
+        self.layout.addWidget(pannel, 0, 1)
 
+    def __setup_pannel(self):
+        pannel = QWidget()
+        layout = QHBoxLayout()
+        pannel.setLayout(layout)
+        self.edit_interface = QLineEdit('0')
+        self.edit_normal = QLineEdit('0, 0, 1')
+        self.edit_depth = QLineEdit('400')
         label_interface  = QLabel('Water Level')
         label_normal  = QLabel('Normal Direction')
-        label_step  = QLabel('Step')
-
-        self.right_edit_interface  = QLineEdit('0')
-        self.right_edit_normal  = QLineEdit('0, 0, 1')
-        self.right_edit_step  = QLineEdit('1')
-
-        layout.addWidget(self.btn_load_image, 0, 0)
-        layout.addWidget(self.btn_load_camera, 0, 1)
-
-        layout.addWidget(label_interface, 1, 0)
-        layout.addWidget(self.right_edit_interface, 1, 1)
-
-        layout.addWidget(label_normal, 2, 0)
-        layout.addWidget(self.right_edit_normal, 2, 1)
-
-        layout.addWidget(label_step, 3, 0)
-        layout.addWidget(self.right_edit_step, 3, 1)
-
-        self.layout.addWidget(window, 0, 1)
-        self.layout.addWidget(pannel, 1, 1)
+        label_step  = QLabel('Water Depth')
+        layout.addWidget(label_interface)
+        layout.addWidget(self.edit_interface)
+        layout.addWidget(label_step)
+        layout.addWidget(self.edit_depth)
+        layout.addWidget(label_normal)
+        layout.addWidget(self.edit_normal)
+        self.layout.addWidget(pannel, 1, 0, 1, 2)
 
 def epipolar_app():
     """
