64a407183243a599676843755b73a1b864b419f9
Yushi Yang <yangyushi1992@icloud.com>
2019-10-08 18:11:56 +0100

    calculate extrinsic parameters from multiple images

--- script/stereo/calibration.py
+++ script/stereo/calibration.py
@@ -3,9 +3,10 @@ import pickle
 import numpy as np
 import re
 import cv2
+import json
 import glob
 import matplotlib.pyplot as plt
-import fish_3d as f3 
+import fish_3d as f3
 import fish_track as ft
 import configparser
 
@@ -17,51 +18,35 @@ setup_sections = ['Stereo', 'Plot', 'Calibration', 'DEFAULT']
 grid_size = float(config['Calibration']['grid_size'])
 win_size = int(config['Calibration']['win_size'])
 corner_number = tuple([int(x) for x in re.split(r'[\s,]+', config['Calibration']['corner_number']) if x])
-use_int = config['Calibration'].get('use_int')
-if use_int:
-    use_int = bool(eval(use_int))
-cameras = {}
 
-for key in config:
-    if key in setup_sections:
+with open(config['Calibration']['order_file'], 'r') as f:
+    calib_orders_json = json.load(f)
+
+cameras = []
+calib_files = []
+calib_orders = []
+camera_names = []
+
+for cam_name in config:
+    if cam_name in setup_sections:
         continue
-    camera_name = key
-    folder_int = config[key]['intrinsic']
-
-
-    if use_int:  # check if there is a pkl file
-        use_int = glob.glob(f'{folder_int}/*.pkl')
-
-    file_ext = config[key]['external']
-    order = config[key]['order']
-    image_fmt = config[key]['format']
+    orders = calib_orders_json[cam_name]
+    folder = config['Calibration']['folder']
+    suffix = config['Calibration']['format']
+    filenames = [f'{folder}/{cam_name}-{i+1}.{suffix}' for i in range(len(orders))]
 
     cam = f3.Camera()
-    if use_int:
-        cam_model = glob.glob(f'{folder_int}/*.pkl')[0]
-        cam.read_int(cam_model)
-        cam.calibrate_ext(
-                ext_image=file_ext,
-                grid_size=grid_size,
-                win_size=(win_size, win_size),
-                order=order,
-                corner_number=corner_number,
-                show=True 
-        )
-    else:
-        cam.calibrate(
-                int_images=[fn for fn in glob.glob(f'{folder_int}/*.{image_fmt}')],
-                ext_image=file_ext,
-                grid_size=grid_size,
-                win_size=(win_size, win_size),
-                order=order,
-                corner_number=corner_number,
-                show=True
-        )
+    cam.read_int(config[cam_name]['intrinsic'])
 
-    cameras.update({camera_name: cam})
+    cameras.append(cam)
+    calib_files.append(filenames)
+    calib_orders.append(orders)
+    camera_names.append(cam_name)
 
-    with open(f'{camera_name}.pkl', 'wb') as f:
+f3.camera.calib_mult_ext(*cameras, *calib_files, *calib_orders, grid_size, corner_number, win_size)
+
+for name, cam in zip(camera_names, cameras):
+    with open(f'{name}.pkl', 'wb') as f:
         pickle.dump(cam, f)
 
 with open(f'cameras.pkl', 'wb') as f:
