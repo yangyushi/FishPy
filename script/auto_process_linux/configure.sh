6440b777dae044e4318c70f396d973cb21f066a9
yushi <yangyushi1992@icloud.com>
2019-10-31 19:16:42 +0000

    minor improvement

--- script/auto_process_linux/configure.sh
+++ script/auto_process_linux/configure.sh
@@ -15,8 +15,8 @@ corner_number="23, 15"  # number of inner corners in the row, column
 # video processing parameters
 background_rolling_length=600
 blur=4
-local=7
-binary_open_size=7
+local=5
+binary_open_size=5
 
 # 2D Tracking parameters
 measure_roi=0
@@ -24,18 +24,18 @@ cam_1_kernels="database/shape_kernels.npy"
 cam_2_kernels="database/shape_kernels.npy"
 cam_3_kernels="database/shape_kernels.npy"
 track_2d_frame_start=0
-track_2d_frame_end=75
+track_2d_frame_end=54000
 track_2d_size_min=5
 track_2d_size_max=25
 track_2d_orientation_number=36
 track_2d_intensity_threshold=0.6
-track_2d_want_plot="True"
+track_2d_want_plot="False"
 
 # 3D Tracking parameters
 track_3d_frame_start=0
-track_3d_frame_end=75
+track_3d_frame_end=54000
 track_3d_sample_size=10
-track_3d_tol_2d=10  # tolarance of reprojection error
+track_3d_tol_2d=3  # tolarance of reprojection error
 track_3d_water_depth=400
 track_3d_water_level=0
-track_3d_want_plot=1
+track_3d_want_plot=0
