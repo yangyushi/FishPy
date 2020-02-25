69e2040031edb5a6181eb5814f1674bece72f297
yushi <yangyushi1992@icloud.com>
2020-02-25 17:35:11 +0000

    collecting all result after successful 2D & 3D tracking

---
+++ script/auto_process_linux/collect.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+
+succeed_2d=1
+for i in {1..3}; do
+    feature="track_2d/features_2d-cam_$i.pkl"
+    shape="track_2d/shapes_2d-cam_$i.npy"
+    if [[ ! -e $feature ]] || [[ ! -e $shape ]]; then
+        succeed_2d=0
+    fi
+done
+
+succeed_3d=1
+for i in {1..3}; do
+    cam="track_3d/cam_$i.pkl"
+    if [[ ! -e $cam ]]; then
+        succeed_3d=0
+    fi
+done
+if [[ ! -e "track_3d/locations_3d.pkl" ]]; then
+    succeed_3d=0
+fi
+
+if [[ $succeed_2d -ne 1 ]]; then
+    echo "2D Tracking Failed"
+    exit 1
+fi
+if [[ $succeed_3d -ne 1 ]]; then
+    echo "3D Tracking Failed"
+    exit 1
+fi
+
+if [[ -d "results" ]]; then
+    rm -rf results
+fi
+mkdir results
+
+python3 script/collect.py results
+
+if [[ $1 == "clear" ]]; then
+    source configure.sh
+    rm -rf "$video_folder"
+fi
