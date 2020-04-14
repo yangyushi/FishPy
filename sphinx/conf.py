b665e11dd6f7e636e591bf4ef8c896f91c638855
Yushi Yang <yangyushi1992@icloud.com>
2020-04-14 11:37:24 +0100

    update documentation

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -47,53 +47,13 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_materialdesign_theme'
+html_theme = 'haiku'
 html_theme_options = {
-    # Specify a list of menu in Header.
-    # Tuples forms:
-    #  ('Name', 'external url or path of pages in the document', boolean, 'icon name')
-    #
-    # Third argument:
-    # True indicates an external link.
-    # False indicates path of pages in the document.
-    #
-    # Fourth argument:
-    # Specify the icon name.
-    # For details see link.
-    # https://material.io/icons/
-    'header_links' : [
-        ('Home', 'index', False, 'home'),
-        ("GitHub", "https://github.com/yangyushi/FishPy", True, 'link'),
-        ("About Me", "https://yangyushi.github.io", True, 'launch'),
-    ],
-
-    # Customize css colors.
-    # For details see link.
-    # https://getmdl.io/customize/index.html
-    #
-    # Values: amber, blue, brown, cyan deep_orange, deep_purple, green, grey, indigo, light_blue,
-    #         light_green, lime, orange, pink, purple, red, teal, yellow(Default: indigo)
-    'primary_color': 'teal',
-    # Values: Same as primary_color. (Default: pink)
-    'accent_color': 'pink',
-
-    # Customize layout.
-    # For details see link.
-    # https://getmdl.io/components/index.html#layout-section
-    'fixed_drawer': True,
-    'fixed_header': True,
-    'header_waterfall': True,
-    'header_scroll': False,
-
-    # Render title in header.
-    # Values: True, False (Default: False)
-    'show_header_title': False,
-    # Render title in drawer.
-    # Values: True, False (Default: True)
-    'show_drawer_title': True,
-    # Render footer.
-    # Values: True, False (Default: True)
-    'show_footer': True
+    "textcolor": "black",
+    "headingcolor": "teal",
+    "linkcolor": "tomato",
+    "visitedlinkcolor": "tomato",
+    "hoverlinkcolor": "cornflowerblue"
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
