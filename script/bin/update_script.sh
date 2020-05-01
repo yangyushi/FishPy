ea1a12dcbd66d4cb5e74c72a86b27cdcf16051bf
Yushi Yang <yangyushi1992@icloud.com>
2020-05-01 23:56:22 +0100

    script update works on mac now

--- script/bin/update_script.sh
+++ script/bin/update_script.sh
@@ -8,9 +8,10 @@ SN=${0##*/}  # script name
 fishpy=${0%/script/bin/$SN}  # path to fishpy
 platform=$(python -mplatform)
 platform=$(expr "$platform" : '\([A-Za-z]*\)-') # linux or darwin
-platform=${platform,,}  # all letters to lowercase
+#platform="${platform,,}"  # all letters to lowercase
+platform="$(echo ${platform} | tr '[:upper:]' '[:lower:]')"
 
-if [[ $platform == "darwin" ]]; then
+if [[ ${platform} == "darwin" ]]; then
     platform="mac"
 fi
 
@@ -22,11 +23,11 @@ should_update=0
 
 # Check if PWD is a auto_process folder
 update_process=1
-needed=(
+needed_process=(
     'auto_link.sh' 'auto_track_2d.sh' 'auto_track_3d.sh'
     'see_trajs.py'
 )
-for fn in "${needed[@]}"; do
+for fn in "${needed_process[@]}"; do
     if [[ ! -e $fn ]]; then
         update_process=0
     fi
@@ -37,10 +38,10 @@ fi
 
 # Check if PWD is a auto_analysis folder
 update_analysis=1
-needed=(
+needed_analysis=(
     'auto_analysis.sh'
 )
-for fn in "${needed[@]}"; do
+for fn in "${needed_analysis[@]}"; do
     if [[ ! -e $fn ]]; then
         update_analysis=0
     fi
@@ -57,20 +58,20 @@ if [[ $should_update -eq 0 ]]; then
 fi
 
 if [[ $update_process -eq 1 ]]; then
-    for fn in "${needed[@]}"; do
+    echo "updating process scripts"
+    for fn in "${needed_process[@]}"; do
         rm "$PWD/$fn"
         cp "$fishpy/script/auto_process_$platform/$fn" "$PWD"
     done
     cp "$fishpy/script/auto_process_$platform/collect.sh" "$PWD"
     rm -rf "$PWD/script"
     cp -r "$fishpy/script/auto_process_$platform/script" "$PWD"
-    echo "Process scripts updated"
     exit 0
 fi
 
 if [[ $update_analysis -eq 1 ]]; then
+    echo "updating analysis scripts"
     rm -rf "$PWD/script"
     cp -r "$fishpy/script/auto_analysis/script" "$PWD"
-    echo "Analysis scripts updated"
     exit 0
 fi
