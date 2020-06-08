9ee6649a897f7b9a5cc93915f64617bd3ca9d7ba
Yushi Yang <yangyushi1992@icloud.com>
2020-06-08 23:38:15 +0100

    more precise output

--- fish_3d/test_greta.cpp
+++ fish_3d/test_greta.cpp
@@ -2,9 +2,6 @@
 #include <iostream>
 
 int main(){
-
-    const bool VERBOSE = false;
-
     tp::Coord2D f0{3, 2};
     tp::Coord2D f1{3, 2};
     tp::Coord2D f2{3, 2};
@@ -34,33 +31,7 @@ int main(){
     StereoTrajs stereo_trajs{temporal_trajs, stereo_links, frames_v3, 2.0};
     stereo_trajs.get_validate_trajs();
 
-    if (VERBOSE){
-        cout << "Validate Trajectories:" << endl;
-        int i = 0;
-        for (auto t : stereo_trajs.trajs_){
-            cout << "SL indices of #"  << ++i << " ("<< t.error_ << ")" << endl;
-            for (auto num : t.id_){
-                cout << num << ", ";
-            }
-            cout << endl;
-        }
-    }
-
     StereoTrajs stereo_trajs_opt = optimise_links_confined(stereo_trajs);
-
-    if (VERBOSE){
-        cout << "After optimisation" << endl;
-        int i = 0;
-        for (auto t : stereo_trajs_opt.trajs_){
-            cout << "SL indices of #"  << ++i << " ("<< t.error_ << ")" << endl;
-            for (auto num : t.id_){
-                cout << num << ", ";
-            }
-            cout << endl;
-        }
-    }
-
-    cout << "Creating Meta Objects (LV1), near root? ";
 
     vector<StereoTrajs> meta_lv1 {stereo_trajs_opt, stereo_trajs_opt, stereo_trajs_opt};
 
@@ -77,26 +48,11 @@ int main(){
         temporal_trajs_meta, stereo_links_meta, frames_meta, meta_lv1, 2.0
     };
 
-    cout << meta_sts_lv1.near_root_ << endl;
-
     meta_sts_lv1.get_validate_trajs();
     MetaSTs<StereoTrajs> meta_sts_lv1_opt = optimise_links_confined(meta_sts_lv1);
 
-    if (VERBOSE){
-        cout << "After optimisation Meta Stereo Trajectories" << endl;
-        int i = 0;
-        for (auto t : meta_sts_lv1_opt.trajs_){
-            cout << "ID of #"  << ++i << " ("<< t.error_ << ")" << endl;
-            for (auto num : t.id_){
-                cout << num << ", ";
-            }
-            cout << endl;
-        }
-    }
+    cout << "Meta LV1, Total Frames: " << meta_sts_lv1_opt.get_total_frames()  << " (should be 12)" << endl;
 
-    cout << "Total Frames: " << meta_sts_lv1_opt.get_total_frames() << endl;
-
-    cout << "Creating Meta Objects (LV2), near root? ";
 
     vector< MetaSTs<StereoTrajs> > meta_lv2 {meta_sts_lv1_opt, meta_sts_lv1_opt, meta_sts_lv1_opt};
 
@@ -114,14 +70,10 @@ int main(){
         temporal_trajs_meta_lv2, stereo_links_meta_lv2, frames_meta_lv2, meta_lv2, 2.0
     };
 
-    cout << meta_sts_lv2.near_root_ << endl;
-
     meta_sts_lv2.get_validate_trajs();
     MetaSTs< MetaSTs<StereoTrajs> > meta_sts_lv2_opt = optimise_links_confined(meta_sts_lv2);
 
-    cout << "Total Frames: " << meta_sts_lv2_opt.get_total_frames() << endl;
-
-    cout << "Creating Meta Objects (LV3), near root? ";
+    cout << "Meta LV2, Total Frames: " << meta_sts_lv2_opt.get_total_frames() << " (should be 36)" << endl;
 
     vector< MetaSTs< MetaSTs<StereoTrajs> > > meta_lv3{
         meta_sts_lv2_opt, meta_sts_lv2_opt, meta_sts_lv2_opt
@@ -137,12 +89,10 @@ int main(){
         temporal_trajs_meta_lv3, stereo_links_meta_lv3, frames_meta_lv3, meta_lv3, 2.0
     };
 
-    cout << meta_sts_lv3.near_root_ << endl;
-
     meta_sts_lv3.get_validate_trajs();
     MetaSTs< MetaSTs< MetaSTs< StereoTrajs> > > meta_sts_lv3_opt = optimise_links_confined(meta_sts_lv3);
 
-    cout << "Total Frames: " << meta_sts_lv3_opt.get_total_frames() << endl;
+    cout << "Meta LV3, Total Frames: " << meta_sts_lv3_opt.get_total_frames() << " (should be 108)" << endl;
 
     st::ProjMat P;
     P << 1, 0, 0, 0,
@@ -153,7 +103,4 @@ int main(){
     array<st::ProjMat, 3> Ps {P, P, P};
     array<st::Vec3D, 3> Os {O, O, O};
     vector<st::Coord3D> trajs = meta_sts_lv3_opt.get_coordinates(Ps, Os);
-    for (auto t : trajs){
-        cout << t.transpose().leftCols(8) << endl;
-    }
 }
