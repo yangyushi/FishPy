256d50a115def80128058f1668480b89b64add27
Yushi Yang <yangyushi1992@icloud.com>
2020-06-10 13:01:05 +0100

    making making CPLEX easier

--- build.sh
+++ build.sh
@@ -1,5 +1,15 @@
 export prefix=/usr/local
 export PY=python3
+export CPLEX_ROOT="/Applications/CPLEX_Studio1210"
+
+# find CPLEX related directories
+Machine=`uname -m | sed "s/_/-/g"`
+Platform=`uname | tr '[:upper:]' '[:lower:]'`
+if [[ $Platform == "darwin" ]]; then
+    Platform="osx"
+fi
+export CPLEX_ARCH=${Machine}_${Platform}
+
 
 cd fish_3d
 make all
