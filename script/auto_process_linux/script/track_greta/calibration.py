0544001b340746a888ed52750b297f690932829e
Yushi Yang <yangyushi1992@icloud.com>
2020-06-11 11:21:00 +0100

    update

--- script/auto_process_linux/script/track_greta/calibration.py
+++ script/auto_process_linux/script/track_greta/calibration.py
@@ -12,38 +12,15 @@ config = configparser.ConfigParser()
 config.read('configure.ini')
 
 setup_sections = ['PostProcess', 'Stereo', 'Temporal', 'Calibration', 'DEFAULT']
-
-grid_size = float(config['Calibration']['grid_size'])
-win_size = int(config['Calibration']['win_size'])
-corner_number = tuple([int(x) for x in re.split(r'[\s,]+', config['Calibration']['corner_number']) if x])
-
-with open(config['Calibration']['order_file'], 'r') as f:
-    calib_orders_json = json.load(f)
-
-cameras = []
-calib_files = []
-calib_orders = []
 camera_names = []
-
 for cam_name in config:
     if cam_name in setup_sections:
         continue
-    orders = calib_orders_json[cam_name]
-    folder = config['Calibration']['folder']
-    suffix = config['Calibration']['format']
-    filenames = [f'{folder}/{cam_name}-{i+1}.{suffix}' for i in range(len(orders))]
-
-    cam = f3.Camera()
-    cam.read_int(config[cam_name]['intrinsic'])
-
-    cameras.append(cam)
-    calib_files.append(filenames)
-    calib_orders.append(orders)
     camera_names.append(cam_name)
 
 # if cam_x in current folder
 is_calibrated = True
-for name, cam in zip(camera_names, cameras):
+for name in camera_names:
     is_calibrated *= f'{name}.pkl' in os.listdir('.')
 
 # if cam_x in ../track_3d
@@ -56,6 +33,34 @@ if (not is_calibrated) and found_in_track3d:
             found_in_track3d = False
 
 if not (is_calibrated or found_in_track3d):
+    grid_size = float(config['Calibration']['grid_size'])
+    win_size = int(config['Calibration']['win_size'])
+    corner_number = tuple([int(x) for x in re.split(r'[\s,]+', config['Calibration']['corner_number']) if x])
+
+    with open(config['Calibration']['order_file'], 'r') as f:
+        calib_orders_json = json.load(f)
+
+    cameras = []
+    calib_files = []
+    calib_orders = []
+    camera_names = []
+
+    for cam_name in config:
+        if cam_name in setup_sections:
+            continue
+        orders = calib_orders_json[cam_name]
+        folder = config['Calibration']['folder']
+        suffix = config['Calibration']['format']
+        filenames = [f'{folder}/{cam_name}-{i+1}.{suffix}' for i in range(len(orders))]
+
+        cam = f3.Camera()
+        cam.read_int(config[cam_name]['intrinsic'])
+
+        cameras.append(cam)
+        calib_files.append(filenames)
+        calib_orders.append(orders)
+        camera_names.append(cam_name)
+
     f3.camera.calib_mult_ext(
         *cameras, *calib_files, *calib_orders,
         grid_size, corner_number, win_size
