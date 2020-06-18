43d0af0c6d77ea3b1ebc3ad641b042119c2de206
Yushi Yang <yangyushi1992@icloud.com>
2020-06-18 12:02:57 +0100

    update
    minor update for movie playing

--- script/bin/play_movie.py
+++ script/bin/play_movie.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
+import sys
 import pickle
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 from mpl_toolkits.mplot3d import Axes3D
 
-with open('movie.pkl', 'rb') as f:
+if len(sys.argv) != 2:
+    exit("fishpy-play filename")
+
+with open(sys.argv[1], 'rb') as f:
     movie = pickle.load(f)
 
 fig = plt.figure()
@@ -12,16 +16,24 @@ ax = fig.add_subplot(111, projection='3d')
 ax.set_xlim(-1000, 1000)
 ax.set_ylim(-1000, 1000)
 ax.set_zlim(-400, 0)
-ax.view_init(elev=75, azim=90)
+ax.view_init(elev=70, azim=90)
 
-scatter = ax.plot([], [], [], 'o', color='lightblue', markeredgecolor='teal')[0]
+scatter = ax.scatter([], [], [], 'o', color='lightblue', edgecolor='teal')
+quiver = ax.quiver3D([], [], [], [], [], [], color='teal')
 
-def update(frame):
+def update(frame_num):
+    global quiver
+    global scatter
+    frame = movie[frame_num]
     if len(frame) > 0:
-        scatter.set_data(*frame.T[:2])
-        scatter.set_3d_properties(frame[:,-1])
-    return [scatter]
+        v = movie.velocity(frame_num)
+        quiver.remove()
+        scatter.remove()
+        quiver = ax.quiver3D(*frame.T, *v.T, color='teal', length=10)
+        scatter = ax.scatter(*frame.T, color='lightblue', edgecolor='teal', s=10)
+    dummy = ax.plot([], [])[0]
+    return [dummy]
 
-ani = FuncAnimation(fig, update, frames=movie, blit=True, interval=1)
+ani = FuncAnimation(fig, update, frames=range(len(movie)), blit=True, interval=10)
 
 plt.show()
