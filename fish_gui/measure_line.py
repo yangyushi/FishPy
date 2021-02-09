9757a7712b9ac4fc575b59bbc83d906736b23be8
Yushi Yang <yangyushi1992@icloud.com>
2021-02-09 23:50:04 +0000

    new function to save/load entire project
    and nice UI layout

--- fish_gui/measure_line.py
+++ fish_gui/measure_line.py
@@ -1,25 +1,30 @@
 import re
-import fish_3d as f3
 import sys
+import json
 import pickle
-from PyQt5.QtWidgets import QWidget, QMainWindow, QPushButton, QLabel,\
-     QGridLayout, QApplication, QHBoxLayout, QLineEdit, QFileDialog,\
-     QDesktopWidget, QMessageBox
-from PyQt5.QtGui import QColor, QVector3D
-from PIL import Image
+import base64
 import numpy as np
+import fish_3d as f3
+from PIL import Image
 import pyqtgraph as pg
 import pyqtgraph.opengl as gl
+from io import BytesIO
 from pyqtgraph.Qt import QtCore, QtGui
+from PyQt5.QtGui import QColor, QVector3D, QIcon
+from PyQt5.QtWidgets import QWidget, QMainWindow, QPushButton, QLabel,\
+     QGridLayout, QApplication, QHBoxLayout, QLineEdit, QFileDialog,\
+     QDesktopWidget, QMessageBox, QAction, qApp
 
 """
 Colors that I like
 """
 TEAL = QColor(14, 110, 100)
+PINK = QColor(254, 176, 191)
 TOMATO = QColor(249, 82, 60)
 CRIMSON = QColor(209, 0, 46)
 LIGHTSEAGREEN = QColor(33, 165, 153)
 CORNFLOWERBLUE = QColor(82, 127, 232)
+LAVENDERBLUSH = QColor(255, 235, 242)
 
 pg.setConfigOptions(imageAxisOrder='row-major')
 
@@ -42,7 +47,7 @@ class Model():
         measurements_3d (list): The 3D line segment representing one fish,
             each element is a numpy array, shape: (2 [points], 3 [xyz])
     """
-    def __init__(self, environment):
+    def __init__(self, environment=None):
         self.images = [None, None, None]
         self.cameras = [None, None, None]
         self.env = environment
@@ -100,18 +105,90 @@ class Model():
 
     @property
     def water_level(self):
-        return float(self.env['z'].text())
+        """
+        default value: 0
+        """
+        if self.env:
+            return float(self.env['z'].text())
+        else:
+            return 0
 
     @property
     def normal(self):
-        normal_str =  self.env['n'].text()
-        normal = re.split(r'[\s,]+', normal_str)
-        normal = [int(n) for n in normal]
-        return normal
+        """
+        default value: (0, 0, 1)
+        """
+        if self.env:
+            normal_str =  self.env['n'].text()
+            normal = re.split(r'[\s,]+', normal_str)
+            normal = [int(n) for n in normal]
+            return normal
+        else:
+            return (0, 0, 1)
 
     @property
     def depth(self):
-        return float(self.env['depth'].text())
+        """
+        default value: 400
+        """
+        if self.env:
+            return float(self.env['depth'].text())
+        else:
+            return 400
+
+    def export_json(self, filename):
+        """
+        Export all the data to a json file
+        """
+        if not self.is_valid:
+            raise RuntimeError("Trying to export invalid model")
+        if filename[-5:] != ".json":
+            filename += ".json"
+        image_strings = []
+        for im in self.images:
+            f = BytesIO()
+            np.savez_compressed(f, data=im)
+            f.seek(0)
+            code = base64.b64encode(f.read())  # bytes
+            string = string = code.decode('utf-8')  # str
+            image_strings.append(string)
+            f.close()
+        data = {
+            'image_strings': image_strings,
+            'cameras': [cam.zip_essential() for cam in self.cameras],
+            'measurements_2d': np.array(self.measurements_2d).tolist(),
+            'measurements_3d': np.array(self.measurements_3d).tolist(),
+            'measurements_3d_errors': np.array(self.measurements_3d_errors).tolist(),
+        }
+        with open(filename, 'w') as f:
+            json.dump(data, f)
+
+    def load_json(self, filename):
+        """
+        Load data from a json file
+        """
+        with open(filename, 'r') as f:
+            data = json.load(f)
+        # restoring images
+        images = []
+        for i, string in enumerate(data['image_strings']):
+            code = base64.b64decode(string)
+            f = BytesIO(code)
+            img = np.load(f, allow_pickle=True)['data']
+            f.close()
+            images.append(img)
+        self.images = images
+        # restoring cameras
+        cameras = [f3.Camera() for i in range(3)]
+        for i, cam in enumerate(cameras):
+            cam.unzip_essential(data['cameras'][i])
+        self.cameras = cameras
+        # restoring measurements
+        self.measurements_2d = [np.array(arr) for arr in data['measurements_2d']]
+        self.measurements_3d = [np.array(arr) for arr in data['measurements_3d']]
+        self.measurements_3d_errors = [
+            np.array(arr) for arr in data['measurements_3d_errors']
+        ]
 
 
 class StereoImageItem(pg.ImageItem):
@@ -164,6 +241,10 @@ class StereoImageItem(pg.ImageItem):
                 self.neighbours.append(stereo_images[si])
 
     def mouseClickEvent(self, event):
+        """
+        For the line segment of current view, plot all its corresponding epipolar lines
+            on the neighbour views.
+        """
         if not self.model.is_valid:
             return
         plot_pens = [
@@ -213,6 +294,7 @@ class Viewer(QMainWindow):
         self.__setup_canvas_2d(index=1, row=0, col=1)
         self.__setup_canvas_2d(index=2, row=1, col=0)
         self.__setup_canvas_3d()
+        self.__setup_menu()
         self.__setup_control()
         for canvas in self.canvas_items:
             canvas.add_neighbour(self.canvas_items)
@@ -224,6 +306,9 @@ class Viewer(QMainWindow):
         qt_rectangle.moveCenter(centre_point)
         self.move(qt_rectangle.topLeft())
 
+        # status bar
+        self.statusBar()
+
         self.show()
 
     def __setup_control(self):
@@ -234,7 +319,12 @@ class Viewer(QMainWindow):
         self.btn_load_camera_items[1].clicked.connect(lambda x: self.__load_camera(1))
         self.btn_load_camera_items[2].clicked.connect(lambda x: self.__load_camera(2))
         self.btn_measure.clicked.connect(self.measure)
-        self.btn_save_3d.clicked.connect(self.export)
+        self.btn_export.clicked.connect(self.export)
+        self.action_exit.triggered.connect(qApp.quit)
+        self.action_export.triggered.connect(self.export)
+        self.action_save.triggered.connect(self.save)
+        self.action_load.triggered.connect(self.load)
+        self.action_env_set.triggered.connect(self.__show_pannel)
 
     def __setup_canvas_2d(self, index, row, col):
         pannel = QWidget()
@@ -248,12 +338,15 @@ class Viewer(QMainWindow):
         # setup the line segment measurement tool
         # making sure the color of the line match the color of the epipolar lines
         line = pg.LineSegmentROI(
-            [(0,0), (10, 10)],
-            pen=pg.mkPen('y', width=2),
-            hoverPen=pg.mkPen('y', width=2)
+            [(0,0), (0, 100)],
+            pen=pg.mkPen(PINK, width=3),
+            hoverPen=pg.mkPen(LAVENDERBLUSH, width=2),
         )
-        line.handles[0]['item'].pen = pg.mkPen(TOMATO, width=4)
-        line.handles[1]['item'].pen = pg.mkPen(LIGHTSEAGREEN, width=4)
+        line.handles[0]['item'].pen = pg.mkPen(TOMATO, width=5)
+        line.handles[1]['item'].pen = pg.mkPen(LIGHTSEAGREEN, width=5)
+        for handle in line.endpoints:
+            handle.currentPen = handle.pen
+        line.hide()
 
         # set the view object to contain the widgets
         view = window.addViewBox(row=0, col=0, lockAspect=True)
@@ -285,25 +378,42 @@ class Viewer(QMainWindow):
         self.btn_load_image_items.append(btn_load_image)
         self.btn_load_camera_items.append(btn_load_camera)
 
+        indices = ['1st', '2nd', '3rd']
+        btn_load_image.setStatusTip(f"Load the image from the {indices[index]} view.")
+        btn_load_camera.setStatusTip(f"Load the camera from the {indices[index]} view.")
+        window.setStatusTip(
+            f"The *undistorted* image and measurements of the {indices[index]} view."
+        )
+
     def __setup_canvas_3d(self):
         pannel = QWidget()
         layout = QGridLayout()
         view_3d = gl.GLViewWidget()
         view_3d.opts['distance'] = 80
         self.btn_measure = QPushButton('Measure 3D Line')
-        self.btn_save_3d = QPushButton('Export 3D Results')
+        self.btn_measure.setStatusTip(
+            "Confirm matched line segments and measure the corresponding 3D line segment."
+        )
+        self.btn_export = QPushButton('Export 3D Results')
+        self.btn_export.setStatusTip(
+            "Export the 3D line segments to a csv file."
+        )
+        view_3d.setStatusTip("View of the 3D line segments.")
 
         # place the widgets
         layout.addWidget(view_3d, 0, 0, 1, 2)
         layout.addWidget(self.btn_measure, 1, 0)
-        layout.addWidget(self.btn_save_3d, 1, 1)
+        layout.addWidget(self.btn_export, 1, 1)
         pannel.setLayout(layout)
 
         self.layout.addWidget(pannel, 1, 1)
         self.canvas_3d = view_3d
 
     def __setup_pannel(self):
-        pannel = QWidget()
+        """
+        the setting pannel
+        """
+        pannel = QWidget(parent=self.parent())
         layout = QHBoxLayout()
         pannel.setLayout(layout)
         self.edit_interface = QLineEdit('0')
@@ -318,9 +428,41 @@ class Viewer(QMainWindow):
         layout.addWidget(self.edit_depth)
         layout.addWidget(label_normal)
         layout.addWidget(self.edit_normal)
-        self.layout.addWidget(pannel, 2, 0, 1, 2)
+        #self.layout.addWidget(pannel, 2, 0, 1, 2)
         env = {'z': self.edit_interface, 'n': self.edit_normal, 'depth': self.edit_depth}
         self.env = env
+        self.pannel = pannel
+
+    def __setup_menu(self):
+        menu_bar = self.menuBar()
+        menu_bar.setNativeMenuBar(False)  # for osx only
+
+        # setting up file menu
+        file_menu = menu_bar.addMenu('&File')
+        save = file_menu.addAction("Save Project")
+        save.setStatusTip("Save all the 2D measurements to a json file.")
+        load = file_menu.addAction("Load Project")
+        load.setStatusTip(
+            "Load all the 2D measurements from a json file and calculate 3D line segments."
+        )
+        export = file_menu.addAction("Export 3D Results")
+        export.setStatusTip("Export all the 3D line segments to a csv file.")
+        save.setShortcut('Ctrl+S')
+        load.setShortcut('Ctrl+L')
+        export.setShortcut('Ctrl+Shift+S')
+        file_menu.addSeparator()
+        exit = file_menu.addAction("Exit")
+        exit.setShortcut('Ctrl+Q')
+        self.action_exit = exit
+        self.action_export = export
+        self.action_save = save
+        self.action_load = load
+
+        # setting up view menu
+        view_menu = menu_bar.addMenu('&View')
+        env_set = view_menu.addAction("Environment Setting")
+        env_set.setStatusTip("Setting the environmental variables.")
+        self.action_env_set = env_set
 
     def __load_image(self, index):
         camera = self.model.cameras[index]
@@ -337,15 +479,21 @@ class Viewer(QMainWindow):
             self.canvas_items[index].setImage(img)
             pos = QtCore.QPoint(img.shape[1]//2, img.shape[0]//2)
             self.canvas_items[index].line.setPos(pos)
+            self.canvas_items[index].line.show()
             self.model.images[index] = img
 
     def __load_camera(self, index):
         camera_name, _ = QFileDialog.getOpenFileName(
                 self, "Select the camera", "", "camera files (*.pkl);;"
                 )
-        with open(camera_name, 'rb') as f:
-            camera = pickle.load(f)
-        self.model.cameras[index] = camera
+        if camera_name:
+            with open(camera_name, 'rb') as f:
+                camera = pickle.load(f)
+            self.model.cameras[index] = camera
+
+    def __show_pannel(self):
+        self.pannel.hide()
+        self.pannel.show()
 
     def __collect_2d_lines(self):
         """
@@ -360,6 +508,10 @@ class Viewer(QMainWindow):
         self.model.measurements_2d.append(measure_2d)
 
     def __refresh_measure_plots(self):
+        """
+        Plotting all the line measurement data from `self.model.measurements_2d`
+            on the canvas of different views
+        """
         for plots_v3 in self.measure_plots_2d:
             for i, plot in enumerate(plots_v3):
                 self.canvas_items[i].view.removeItem(plot)
@@ -376,6 +528,10 @@ class Viewer(QMainWindow):
             self.measure_plots_2d.append(plots_v3)
 
     def __refresh_3d_plot(self):
+        """
+        Plotting all the line measurement data from `self.model.measurements_2d`
+            on the canvas of different views
+        """
         for item in self.measure_plots_3d:
             self.canvas_3d.removeItem(item)
         self.measure_plots_3d.clear()
@@ -391,32 +547,75 @@ class Viewer(QMainWindow):
             self.canvas_3d.addItem(plt)
             self.measure_plots_3d.append(plt)
 
+    def __check_and_get_save_name(self, suffix, caption=""):
+        """
+        Check the existance of valid result and get a save name
+
+        Args:
+            suffix (str): the extension of the file (e.g. csv, json).
+            caption (str): the caption on the file dialog
+        """
+        if not self.model.is_valid:
+            warn("Please Generate Measurements First")
+            return
+        if len(self.model.measurements_3d) == 0:
+            warn("Please Generate Measurements First")
+            return
+        save_name, _ = QFileDialog.getSaveFileName(
+            caption="Save 3D line measurements",
+            filter=f"{suffix.upper()} files (*.{suffix})"
+        )
+        return save_name
+
     def measure(self):
         """
         Adding a permenant line plots for the line segments from each view,
         Calculate the 3D lines based on the 2D line segments.
         Renderling the 3D lines in the 3D view.
         """
-        self.__collect_2d_lines()
-        self.__refresh_measure_plots()
         if self.model.is_valid:
+            self.__collect_2d_lines()
+            self.__refresh_measure_plots()
             self.model.calculate()
+            self.__refresh_3d_plot()
+        else:
+            warn("Please load the images and cameras")
+
+    def save(self):
+        save_name = self.__check_and_get_save_name(
+            'json', caption="Save Project as Json"
+        )
+        if save_name:
+            self.model.export_json(save_name)
+
+    def load(self):
+        filename, _ = QFileDialog.getOpenFileName(
+                self, "Select the Project File", "", "json files (*.json);;"
+                )
+        if filename:
+            self.model.load_json(filename)
+            for i, img in enumerate(self.model.images):
+                pos = QtCore.QPoint(img.shape[1]//2, img.shape[0]//2)
+                self.canvas_items[i].line.setPos(pos)
+                self.canvas_items[i].line.show()
+                self.canvas_items[i].setImage(img)
+            self.__refresh_measure_plots()
             self.__refresh_3d_plot()
 
     def export(self):
-        save_name, _ = QFileDialog.getSaveFileName(
-            caption="Save 3D line measurements",
-            filter="CSV files (*.csv)"
+        save_name = self.__check_and_get_save_name(
+            'csv', caption="Save 3D line measurements to CSV"
         )
-        data = np.array(self.model.measurements_3d)  # (n, 2, 3)
-        error = np.array(self.model.measurements_3d_errors)[:, :, np.newaxis]  # (n, 2, 1)
-        data = np.concatenate((data, error), axis=-1)  # (n, 2, 4)
-        n = data.shape[0]
-        data = data.reshape((n, 8), order='C')  # last axis index changing fastest
-        header = "X1,Y1,Z1,Error1,X2,Y2,Z2,Error2"
-        np.savetxt(
-            save_name, data, delimiter=',', header=header, comments=""
-        )
+        if save_name:
+            data = np.array(self.model.measurements_3d)  # (n, 2, 3)
+            error = np.array(self.model.measurements_3d_errors)[:, :, np.newaxis]  # (n, 2, 1)
+            data = np.concatenate((data, error), axis=-1)  # (n, 2, 4)
+            n = data.shape[0]
+            data = data.reshape((n, 8), order='C')  # last axis index changing fastest
+            header = "X1,Y1,Z1,Error1,X2,Y2,Z2,Error2"
+            np.savetxt(
+                save_name, data, delimiter=',', header=header, comments=""
+            )
 
 
 def line_measure_app():
