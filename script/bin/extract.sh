80bfcc43a80e7c632ba952bc29f4a51ac782a8f6
Yushi Yang <yy17363@bristol.ac.uk>
2021-02-10 21:00:28 +0000

    allow delay when extracting frames

--- script/bin/extract.sh
+++ script/bin/extract.sh
@@ -7,10 +7,15 @@ if [[ -n $1 ]]; then
     format=$1
 fi
 
+delay=0
+if [[ -n $2 ]]; then
+    delay=$2
+fi
+
 for fn in ./*.${format}; do
     echo getting first frame from $fn
     img_name="${fn%.${format}}.png"
     if [[ ! -e $img_name ]]; then
-        ffmpeg -y -i ${fn} -vframes 1 ${fn%.${format}}.png  &> /dev/null
+        ffmpeg -ss ${delay} -y -i ${fn} -vframes 1 ${fn%.${format}}.png  &> /dev/null
     fi
 done
